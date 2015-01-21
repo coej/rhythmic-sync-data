e02d872efc06833a64d9845dc93d0cb874c9fee2
Chris Jenkins <chrisoej@gmail.com>
2015-01-21 16:15:05 -0500

    added participant id as a data column

--- clean_data.py
+++ clean_data.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 
 from __future__ import print_function
-import os, csv
+import os, csv, re
 
 path = os.path.join(os.getcwd(), 'data_csv_raw')
 
@@ -15,21 +15,30 @@ if not os.path.exists(new_dir):
     os.makedirs(new_dir)
 
 bad_csv_keywords = ['[', 'taskRunCount', 'end,end,end', 'IntervalOut,X,X,0']
-csv_header = "taskRunCount,taskID,taskName,i,channel,pitch,velocity,microseconds"
-
+csv_header = ('pid,taskRunCount,taskID,taskName,i,'
+              'channel,pitch,velocity,microseconds')
 
 for old_path in csvfiles:    
     
-    new_path = os.path.join(new_dir, os.path.basename(old_path))    
+    filename = os.path.basename(old_path)
+    new_path = os.path.join(new_dir, filename)    
     
+    participant_id = re.match(r"^P-(...).csv$", filename).group(1)
+    assert participant_id.isdigit() and len(participant_id) == 3
+    pid_num = str(int(participant_id)) # remove leading zeroes
+
     with open(old_path) as oldfile, open(new_path, 'w') as newfile:
         
-        newfile.write(csv_header + '\n')        
+        newfile.write(csv_header + '\n')
+
         for linecount, line in enumerate(oldfile):
             if any(word in line for word in bad_csv_keywords):
                 continue
             elif not line[0].isdigit():
                 continue
+
+            #adding participant number from filename as data column
+            line += pid_num
 
             # fixing error in construction of some data rows and using
             # a numeric key to indicate a particular kind of timestamp
@@ -51,7 +60,7 @@ cleaned_csvfiles = [os.path.join(cleaned_path, f)
 
 #confirm proper structure
 dlists = [list(csv.DictReader(open(f))) for f in cleaned_csvfiles]
-assert all([dl[0].keys() == ['taskRunCount', 'i', 'microseconds', 'taskID',
-                            'pitch', 'velocity', 'taskName', 'channel']
+assert all([set(dl[0].keys()) == {'pid', 'taskRunCount','taskID','taskName',
+                                  'i','channel','pitch','velocity','microseconds'}
             for dl in dlists])
     
